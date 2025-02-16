5fb9a4270f940e268ee4ed02f44df85835bc4eb7
Danil <28042001a@gmail.com>
2025-02-16 16:16:47 +0500

    Create components button

--- src/pages/main-page/components/main-packages/components/package-item/package-item.jsx
+++ src/pages/main-page/components/main-packages/components/package-item/package-item.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/button';
+
 import styles from './package-item.module.scss';
 
 export const PackageItem = ({ package: { id, title, price, description } }) => {
@@ -8,6 +10,9 @@ export const PackageItem = ({ package: { id, title, price, description } }) => {
         <span className={styles.packagePrice}>{price}</span>
         <span className={styles.packageDescription}>{description}</span>
       </div>
+      <Button variant="secondary" additionalClassname={styles.packageBtn} disabled>
+        Оставить заявку
+      </Button>
     </div>
   );
 };
