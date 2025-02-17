bd7c2772df968db204c8f2e5406bf590383454a5
Danil <28042001a@gmail.com>
2025-02-17 14:38:54 +0500

    Fix naming

--- src/pages/main-page/components/main-packages/components/package-item/package-item.jsx
+++ src/pages/main-page/components/main-packages/components/package-item/package-item.jsx
@@ -10,7 +10,7 @@ export const PackageItem = ({ package: { id, title, price, description } }) => {
         <span className={styles.packagePrice}>{price}</span>
         <span className={styles.packageDescription}>{description}</span>
       </div>
-      <Button additionalClassname={styles.btn}>Оставить заявку</Button>
+      <Button>Оставить заявку</Button>
     </div>
   );
 };
